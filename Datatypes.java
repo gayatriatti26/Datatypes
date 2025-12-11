51c654ae4616a6b40ef82a2032944a18515a584e
GAYATRI ATTI <attigayatri1726@gmail.com>
2025-12-11 20:36:37 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+class Datatypes {
+    public static void main(String[] args) {
+        // int num1=9;
+        byte by=127;
+        short sh=558;
+        long l=5854l;
+        float f=5.8f;
+        double d=6.5;
+        char c='k';
+        boolean b=true; //
+      //  int num1 = 1000000000; //
+      // double num1=56; //
+      char C ='a';
+      C++;
+        System.out.println(C);
+    }
+}
